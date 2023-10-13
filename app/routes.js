abf3c0d8da170124571a4c7c4208e074a6fbc1e7
Enor Eseme Anidi-Ryz <hello@enoranidi.com>
2023-10-13 15:55:42 +0100

    routes and statuses updates

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,5 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+require('./routes/assess/v6/routes.js')(router);
+
---
+++ app/routes/assess/v6/routes.js
@@ -0,0 +1,9 @@
+module.exports = function (router) {
+    var version = "v6";
+    router.get('/assess/' + version + '/update-status', function (req, res) {
+        res.render('/assess/' + version + '/update-status')
+    });
+    router.post('/assess/' + version + '/update-status', function (req, res) {
+        res.redirect('status-history')
+  });
+}
