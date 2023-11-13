0919a64d8098ad04c569781bb2565b1075dfa382
Enor Eseme Anidi-Ryz <hello@enoranidi.com>
2023-11-13 10:32:16 +0000

    Updates to the withdrawal journeys

--- app/routes.js
+++ app/routes.js
@@ -11,6 +11,19 @@ require('./routes/assess/v6/routes.js')(router);
 
 
 
+router.get('/refer/features/withdrawal/v1/withdraw-referral', function (req, res) {
+  res.render('/refer/features/withdrawal/v1/withdraw-referral')
+});
+
+router.post('/refer/features/withdrawal/v1/withdraw-referral', function (req, res) {
+   const withdrawalReason = req.session.data['reason-category']
+
+     if (withdrawalReason == 'Personal and health'){
+       res.redirect('personal-health')
+     } else {
+       res.redirect('motivation-behaviour')
+     }
+ });
 
 
 router.get('/assess/deselect/withdraw-referral', function (req, res) {
